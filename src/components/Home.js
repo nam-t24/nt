99a36c83ab5638dc455ea4bc68389bdf7b28aac3
nam-t24 <namtruong831@gmail.com>
2021-11-15 11:35:19 -0600

    Activiate github pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home(){
     return(
         <div className = "bg-blue-200 w-3/4">
-            Hello World!!!!!!
+            Hello World!!!!!! Github pages
         </div>
     );
 }
