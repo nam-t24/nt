314008d6ebfcbc4c27e836da194ed5e481e15018
nam-t24 <namtruong831@gmail.com>
2021-11-15 17:22:11 -0600

    add all assets

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 
 function Home(){
     return(
-        <div className = "bg-blue-200 w-3/4">
-            <Link to="/resume">RESUME</Link>
+        <div className = "bg-blue-200 flex justify-between p-4">
+            <div>
+                Home
+            </div>
+            <div>
+                Resume
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        'OldLavender': '#6D6875',
+        'EnglishLavender': '#B5838D',
+        'PastelPink': '#E5989B',
+        'Melon':'#FFB4A2',
+        'Apricot' : '#FFCDB2',
+      })
+    },
   },
   variants: {},
   plugins: [],
